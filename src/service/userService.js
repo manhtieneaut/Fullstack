b21f6522b64b0a6fc722f44c4c1d6ed9649af9ab
TienCNTT <20200892@eaut.edu.vn>
2023-08-29 21:03:48 +0700

    hash password

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2"
-
+import bcrypt from "bcryptjs"
+var salt = bcrypt.genSaltSync(10);
 // create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -7,14 +8,14 @@ const connection = mysql.createConnection({
     database: 'jwt'
 });
 
-// const hashPassword = (password) => {
-//     let hashPassword = bcrypt.hashSync(password, salt)
-//     return hashPassword;
-// }
+const hashPass = (password) => {
+    let hashPassword = bcrypt.hashSync(password, salt)
+    return hashPassword;
+}
 
 const createNewUser = (email, password, username) => {
     connection.query(
-        'INSERT INTO users(email,password,username) VALUES( ? , ? , ? )', [email, password, username],
+        'INSERT INTO users(email,password,username) VALUES( ? , ? , ? )', [email, hashPass(password), username],
         function (err, results) {
             if (err) {
                 console.log(err)
