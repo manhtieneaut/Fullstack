6131f8716c0ae1ce9e406e991d6d8bd8c1cc7de3
TienCNTT <20200892@eaut.edu.vn>
2023-09-17 18:21:44 +0700

    add user by sequelize

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,9 @@
 import mysql from "mysql2/promise"
 import bluebird from "bluebird"
 import bcrypt from "bcryptjs"
+import db from "../models";
+
+
 var salt = bcrypt.genSaltSync(10);
 
 // create the connection to database
@@ -17,10 +20,19 @@ const hashPass = (password) => {
 }
 
 const createNewUser = async (email, password, username) => {
-    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
+    // const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
     try {
-        const [rows, fields] = await connection.execute('INSERT INTO users(email,password,username) VALUES( ? , ? , ? )', [email, hashPass(password), username]);
-        return rows
+        // const [rows, fields] = await connection.execute('INSERT INTO users(email,password,username) VALUES( ? , ? , ? )', [email, hashPass(password), username]);
+        // return rows
+
+        await db.User.create(
+            {
+                email:email,
+                password:hashPass(password),
+                username:username
+            }
+        )
+
     } catch (error) {
         console.log(">>>check error", error)
     }
